b832d5c6d9de6c834dbc94429185b8c14ecdc7d3
kmiit <61952405+kmiit@users.noreply.github.com>
2024-09-10 02:33:14 +0800

    Remove unused css

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,7 +2,7 @@
   <div class="logo">
       <img src="../assets/Logo.svg" />
   </div>
-  <div class="Main">
+  <div class="main">
     <h1>Welcome To Aris Go Project Official Website!</h1>
     <h2>The whole project is still under developing, please wait paciently</h2>
     <p>As you see, I am doing all the things alone</p>
