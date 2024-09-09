30289fcfb879e3993accfff8b996fa34cff0b770
kmiit <61952405+kmiit@users.noreply.github.com>
2024-09-10 01:57:32 +0800

    Add logo

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
+  <div class="logo">
+      <img src="../assets/Logo.svg" />
+  </div>
+  <div class="Main">
     <h1>Welcome To Aris Go Project Official Website!</h1>
-    <h2>The website is still under developing, please wait paciently</h2>
-    <p><s>I don't know whether this project will keep alive</s></p>
+    <h2>The whole project is still under developing, please wait paciently</h2>
+    <p>As you see, I am doing all the things alone</p>
+    <p><s>So I don't know whether this project will keep alive</s></p>
   </div>
 </template>
-
